7c8925ed46ce622baa3d17b57fda8ff85b968c0f
loganeworth <loganeworth@gmail.com>
2022-12-22 17:30:09 -0600

    Edited readme file

--- Develop/script.js
+++ Develop/script.js
@@ -27,8 +27,7 @@ function generatePassword() {
   let passwordSpecialcharacters = confirm('Include special characters?');
   let specialcharacters = '!@#$%&*_';
   console.log(passwordPrompt)
-  
-//below is testing code
+
 
   if (passwordUpper) {
     allpossible += uppercase
@@ -55,32 +54,6 @@ function generatePassword() {
   }
   
   return genPassword
-  
-
-// commented out code, will comeback to
-
-  // if (passwordUpper) {
-  //   allpossible.push(passwordUpper)
-  // }
-  
-  // if (passwordLower) {
-  //   allpossible.push(passwordLower)
-  // }
-  
-  // if (passwordNumeric) {
-  //   allpossible.push(passwordNumeric)
-  // }
-  
-  // if (passwordSpecialcharacters) {
-  //   allpossible.push(passwordSpecialcharacters)
-  // }
-
-  // for (let index = 0; index < passwordLength; index++) {
-  //   let randomIndex = Math.floor(Math.random()*allpossible.length)
-  //     console.log(allpossible[randomIndex])
-  // }
-
-// comeback to above commented out code
 
 
 }
@@ -99,144 +72,4 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// NEED TO COMMENT OUT BELOW
-// Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// function generatePassword() {
-  
-// }
-
-// // Write password to the #password input
-// var password = generatePassword();
-// function writePassword() {
-//   // var password = generatePassword();
-//   let passwordPrompt = prompt('How long would you like your password? 8-128 characters');
-//   let passwordLength = parseInt(passwordPrompt)
-//   if (passwordPrompt < 8 || passwordPrompt > 128 || isNaN(passwordPrompt)) {
-//     alert("Check your length!!")
-    
-//   }
-
-//   let allpossible = []
-
-  
-  
-//   let passwordUpper = confirm('Include upper case?');
-//   let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  
-//   let passwordLower = confirm('Include lower case');
-//   let lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  
-//   let passwordNumeric = confirm('Include numbers?');
-//   let numeric = '0123456789';
-  
-//   let passwordSpecialcharacters = confirm('Include special characters?');
-//   let specialcharacters = '!@#$%&*_';
-//   console.log(passwordPrompt)
-//   if (passwordUpper) {
-//     allpossible.push(passwordUpper)
-//   }
-  
-//   if (passwordLower) {
-//     allpossible.push(passwordLower)
-//   }
-  
-//   if (passwordNumeric) {
-//     allpossible.push(passwordNumeric)
-//   }
-  
-//   if (passwordSpecialcharacters) {
-//     allpossible.push(passwordSpecialcharacters)
-//   }
-
-//   for (let index = 0; index < passwordLength; index++) {
-//     let randomIndex = Math.floor(Math.random()*allpossible.length)
-//       console.log(allpossible[randomIndex])
-//   }  
-// }
-//   var passwordText = document.querySelector("#password");
-  
-
-//   passwordText.value = password;
-
-
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-//ABOVE IS MAIN OF WHAT WAS WORKING ON
-
-// let allpossible
-
-// let passwordLength = prompt('How long would you like your password? 8-128 characters');
-
-
-// let passwordUpper = confirm('Include upper case?');
-// let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-// let passwordLower = confirm('Include lower case');
-// let lowercase = 'abcdefghijklmnopqrstuvwxyz';
-
-// let passwordNumeric = confirm('Include numbers?');
-// let numeric = '0123456789';
-
-// let passwordSpecialcharacters = confirm('Include special characters?');
-// let specialcharacters = '!@#$%&*_';
-
-// if (passwordUpper) {
-//   allpossible += uppercase
-// }
-
-// if (passwordLower) {
-//   allpossible += lowercase
-// }
-
-// if (passwordNumeric) {
-//   allpossible += numeric
-// }
-
-// if (passwordSpecialcharacters) {
-//   allpossible += specialcharacters
-// }
-
-// for (let index = 0; index < passwordLength; index++) {
-// let randomIndex = Math.floor(Math.random()*allpossible.length)
-//   console.log(allpossible[randomIndex])
-// }
-
-// function pass() {
-//   let display = document.querySelector("#password")
-//   display.value = allpossible
-// }
-
-// generateBtn.addEventListener("click", writePassword);
-// let pass = alert(randomIndex[0], randomIndex[1], randomIndex[2], randomIndex[3], randomIndex[4], randomIndex[5], randomIndex[6], randomIndex[7])
-
-// alert(randomIndex)
-
-// if (allpossible) {
-//   alert(allpossible[randomIndex])
-// }
-
-// let allpossible = 
-
-//stick the string the on the page and then
-
-// if (passwordUpper) {
-// alert('you chose uppercase')
-// }
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # password-generator-HW3
+
+Second homework assignment, Javascript.
+
+## Description
+
+The purpose of this project was to apply applicable learnings from class to finish source code of creating a password generator.
+
+## Installation
+
+To open this website, copy and paste the live link into a browser. Then, click the generate password button. Follow the prompts and at the end your password will be displayed in the generate password box.
+
+## Screen-Capture
+
+![Password generator webpage includes a box and a generate password button.](/Assets/03-javascript-homework-demo.png)
+
+## Usage
+
+The usage of this assignment was to apply Javascript to the provided and finish the password generator.
+
+## Credits
+
+I would like to thank the University of Minnesota's Full Stack Coding Bootcamp for providing the source code.
